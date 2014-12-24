f94d45177db1255cdce2c8920d3f4ae2545051b0
Konstantin Koval <konstantin@rocketfarm.no>
2014-12-24 21:20:34 +0100

    add new CoreData WatchKit project

---
+++ CoreData/CoreData/MasterViewController.swift
@@ -0,0 +1,85 @@
+//
+//  MasterViewController.swift
+//  CoreData
+//
+//  Created by Konstantin Koval on 24/12/14.
+//  Copyright (c) 2014 Konstantin Koval. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+  var objects = [AnyObject]()
+
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.navigationItem.leftBarButtonItem = self.editButtonItem()
+
+    let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+    self.navigationItem.rightBarButtonItem = addButton
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  func insertNewObject(sender: AnyObject) {
+    objects.insert(NSDate(), atIndex: 0)
+    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+  }
+
+  // MARK: - Segues
+
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "showDetail" {
+        if let indexPath = self.tableView.indexPathForSelectedRow() {
+            let object = objects[indexPath.row] as NSDate
+        (segue.destinationViewController as DetailViewController).detailItem = object
+        }
+    }
+  }
+
+  // MARK: - Table View
+
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return objects.count
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+
+    let object = objects[indexPath.row] as NSDate
+    cell.textLabel!.text = object.description
+    return cell
+  }
+
+  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+  }
+
+  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+        objects.removeAtIndex(indexPath.row)
+        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    }
+  }
+
+
+}
+
