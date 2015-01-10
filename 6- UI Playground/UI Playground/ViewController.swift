bed7f22f803229fd9072efc337978ac2c8f9f02d
Konstantin Koval <konstantin@rocketfarm.no>
2015-01-10 11:36:31 +0100

    add new Project

---
+++ 6- UI Playground/UI Playground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UI Playground
+//
+//  Created by Konstantin Koval on 10/01/15.
+//  Copyright (c) 2015 Kostiantyn Koval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
