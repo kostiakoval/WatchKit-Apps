ef58381bf8a89f0b099a7ef63fb493c68ef6de72
Konstantin Koval <konstantin@rocketfarm.no>
2015-01-17 08:23:45 +0100

    add table navigation for all controllers

---
+++ 6- UI Playground/UI Playground WatchKit Extension/ContentTableController.swift
@@ -0,0 +1,48 @@
+//
+//  ContentTableController.swift
+//  UI Playground
+//
+//  Created by Konstantin Koval on 17/01/15.
+//  Copyright (c) 2015 Kostiantyn Koval. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class RowController: NSObject {
+  
+  @IBOutlet weak var titleLabel: WKInterfaceLabel!
+}
+
+class ContentTableController: WKInterfaceController {
+  
+  @IBOutlet weak var table: WKInterfaceTable!
+  let objects = ["Vertical", "Horizontal", "Mixed",
+    "Pos - Vertical", "Pos - Horizontal",
+    "Size - Fit Content","Size - Relative","Size - Fixed", "Size - Fix One",
+    "Order", "Long Scroll"]
+  
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    table.setRowTypes(Array(count: objects.count, repeatedValue:"Row"))
+    for (index, obj) in enumerate(objects) {
+      let controller = table.rowControllerAtIndex(index) as RowController
+      controller.titleLabel.setText(obj)
+    }
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
+  override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+    pushControllerWithName(objects[rowIndex], context: nil)
+  }
+  
+}
