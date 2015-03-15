49a7d3b339efbf98fe59613979ba17fa6507f0a8
Konstantin Koval <konstantin@rocketfarm.no>
2015-03-15 22:19:13 +0100

    Add more animation examples

---
+++ 10- Animation/Animation WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Animation WatchKit Extension
+//
+//  Created by Konstantin Koval on 15/03/15.
+//  Copyright (c) 2015 Kostiantyn Koval. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
